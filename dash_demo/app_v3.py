25fa1bbb4d5a184d9a425ff0fcb9963ec30cc279
Karl <karlpmm@gmail.com>
2024-12-05 11:09:06 -0300

    fazendo gambiarra pra fazer funcionar

--- dash_demo/app_v3.py
+++ dash_demo/app_v3.py
@@ -5,15 +5,162 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import datetime
-from dash_demo.helpers.graphs import *
-from dash_demo.helpers.custom_elements import *
-from dash_demo.statics.visuals import *
+#from dash_demo.helpers.graphs import *
+#from dash_demo.helpers.custom_elements import *
+#from dash_demo.statics.visuals import *
 import folium
 from streamlit_folium import folium_static
 
+## GAMBIARRAS
+css = """
+.legend {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
+.legend-item {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+}
+.legend-color {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+}
+div[data-testid="metric"] {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+/* Centralizar o texto dentro das métricas */
+.metric-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 10px;
+    margin: 10px;
+}
+.metric-title {
+    font-size: 2em;
+    font-weight: bold;
+}
+.metric-value {
+    font-size: 4em;
+    font-weight: bold;
+}
+h1, h2, h3, h4, h5, h6 {
+    text-align: center;
+}
+"""
+st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
-with open("dash_demo/statics/style.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+icon_dict = {
+    'Escola': 'glyphicon glyphicon-education',
+    'UBS': 'glyphicon glyphicon-plus-sign',
+    'Povos Originários': 'glyphicon glyphicon-home',
+    'Outros': 'glyphicon glyphicon-map-marker'
+}
+color_dict = {
+    'Online': 'green',
+    'Offline': 'red',
+    'Atenção': 'orange'
+}
+
+
+legenda1 = """
+<div class="legend">
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(190, 215, 51, 1);"></div>
+        <span>Ativo</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(0, 159, 227, 1);"></div>
+        <span>Bloqueados</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(32, 46, 57, 1);"></div>
+        <span>Terminated</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(255, 165, 0, 1);"></div>
+        <span>Grace</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(0, 100, 0, 1);"></div>
+        <span>Reduced</span>
+    </div>
+</div>
+"""
+
+legenda2 = """
+<div class="legend">
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(190, 215, 51, 1);"></div>
+        <span>Online</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(0, 159, 227, 1);"></div>
+        <span>Atencao</span>
+    </div>
+    <div class="legend-item">
+        <div class="legend-color" style="background-color: rgba(32, 46, 57, 1);"></div>
+        <span>Offline</span>
+    </div>
+</div>
+"""
+
+import plotly.express as px
+
+c1 = 'rgba(32, 46, 57, 1)'
+c2 = "rgba(0, 159, 227, 1)"
+c3 = "rgba(190, 215, 51, 1)"
+c4 = "rgba(255, 165, 0, 1)"  # Orange color
+c5 = "rgba(0, 100, 0, 1)"
+cores = [c3, c2, c1, c4, c5]
+
+def grafico_status(df, title):
+    df = df[df['new_tipo'] == title]
+    fig = px.pie(df, values='old_circuit_id', names='final_status',
+                  color='final_status',
+                  color_discrete_map={'Ativo':c3, 'Bloqueados':c2, 'Terminated': c1, 'Grace':c4, 'Reduced':c5})
+    fig.update_layout(
+        title = {'text':title, 'x':0.3},
+        autosize=False, width=400, height=320,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        showlegend=False
+    )
+    fig.update_traces(textinfo='value+percent', textposition='inside')
+    return fig
+
+def grafico_atividade(df, title):
+    df = df[df['new_tipo'] == title]
+    fig = px.pie(df, values='old_circuit_id', names='categoria',
+                  color='categoria',
+                  color_discrete_map={'Online':c3, 'Atenção':c2, 'Offline': c1, 'Grace':c4, 'Reduced':c5})
+    fig.update_layout(
+        title = {'text':title, 'x':0.3},
+        autosize=False, width=400, height=320,
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        showlegend=False
+    )
+    fig.update_traces(textinfo='value+percent', textposition='inside')
+    return fig
+
+
+def central_metric(label, value):
+    st.markdown(f"""
+        <div class="metric-container">
+            <div class="metric-title">{label}</div>
+            <div class="metric-value">{value}</div>
+        </div>
+    """, unsafe_allow_html=True)
 
 
 # Function to get data from MySQL
@@ -87,8 +234,7 @@ def analises_iniciais(data):
 
     st.title('Pontos Existentes na Base e Status')
     # Legenda do Gráfico
-    with open("dash_demo/statics/legenda1.html") as f:
-        st.markdown(f.read(), unsafe_allow_html=True)
+    st.markdown(legenda1, unsafe_allow_html=True)
 
     g1, g2, g3, g4 = st.columns(4)
     # Visualizando gráficos
@@ -117,8 +263,7 @@ def analise_atividade(data):
 
     st.title('Atividade dos Pontos')
     # Legenda do Gráfico
-    with open("dash_demo/statics/legenda2.html", encoding='latin-1') as f:
-        st.markdown(f.read(), unsafe_allow_html=True)
+    st.markdown(legenda2, unsafe_allow_html=True)
     g1, g2, g3, g4 = st.columns(4)
     # Visualizando gráficos
     with g1:
